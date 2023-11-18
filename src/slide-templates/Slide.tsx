2e891c1f1314dd7b512318659c86f072bd6ec3bb
aarkue <mail@aarkue.eu>
2023-11-19 00:41:36 +0100

    Remove slide border

--- src/slide-templates/Slide.tsx
+++ src/slide-templates/Slide.tsx
@@ -39,7 +39,7 @@ export default function Slide(props: SlideProps) {
         data-background-image={props.backgroundImage}
         ref={sectionRef}
         id={slideIndex !== undefined ? `slide-${slideIndex + 1}` : undefined}
-        className={classNames("w-full h-full border", {
+        className={classNames("w-full h-full", {
           "!relative !top-0 !left-0": isPDF,
         })}
       >
