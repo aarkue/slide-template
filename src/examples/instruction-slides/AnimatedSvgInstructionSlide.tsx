ef65a3e6f8c72c8eb8b39d03f15e1adff8978526
aarkue <mail@aarkue.eu>
2023-11-19 00:41:53 +0100

    Updates to instruction example slides

--- src/examples/instruction-slides/AnimatedSvgInstructionSlide.tsx
+++ src/examples/instruction-slides/AnimatedSvgInstructionSlide.tsx
@@ -18,12 +18,14 @@ export default function AnimatedSvgInstructionSlide() {
               SVGs with a draw effect
             </li>
             <li>
+              Powered by{" "}
+              <a href="https://github.com/maxwellito/vivus" target="_blank" referrerPolicy="no-referrer">
+                vivus.js
+              </a> 🔌
+            </li>
+            <li>
               Also works great with{" "}
-              <a
-                href="https://excalidraw.com/"
-                target="_blank"
-                referrerPolicy="no-referrer"
-              >
+              <a href="https://excalidraw.com/" target="_blank" referrerPolicy="no-referrer">
                 excalidraw
               </a>{" "}
               sketches
@@ -36,9 +38,7 @@ export default function AnimatedSvgInstructionSlide() {
             containerClassName={"w-full mt-8"}
             className={"h-[16rem] object-contain mx-auto"}
           />
-          <span className="text-xs text-gray-600 text-center block">
-            Click on the image to restart the animation
-          </span>
+          <span className="text-xs text-gray-600 text-center block">Click on the image to restart the animation</span>
         </div>
       }
     />
--- src/examples/instruction-slides/CustomizationInstructionSlide.tsx
+++ src/examples/instruction-slides/CustomizationInstructionSlide.tsx
@@ -8,49 +8,38 @@ export default function CustomizationInstructionSlide() {
       content={
         <div className="h-full w-full">
           <ul>
-            <li>
+            <li className="mb-4">
               All the customization power of{" "}
-              <a
-                href="https://react.dev/"
-                target="_blank"
-                referrerPolicy="no-referrer"
-              >
+              <a href="https://react.dev/" target="_blank" referrerPolicy="no-referrer">
                 React
               </a>{" "}
               and{" "}
-              <a
-                href="https://tailwindcss.com//"
-                target="_blank"
-                referrerPolicy="no-referrer"
-              >
+              <a href="https://tailwindcss.com//" target="_blank" referrerPolicy="no-referrer">
                 Tailwind
               </a>
             </li>
-            <br />
             <li>Create slides as React components</li>
             <li>
-              Construct custom{" "}
-              <span className="text-pink-500">slide templates</span>
+              Construct custom <span className="text-pink-500">slide templates</span>
             </li>
             <li className="relative">
-              <span className="italic font-black underline tracking-wider text-fuchsia-400 hover:text-fuchsia-500 -rotate-6 inline-block">
+              <span className="italic font-black underline tracking-wider text-fuchsia-400 hover:text-fuchsia-500 -rotate-3 inline-block">
                 Style
               </span>{" "}
               everything in a breeze with Tailwind CSS
-              <span className="absolute text-xl right-0 -top-4 rotate-6">
-                <span className="tracking-tight font-serif">Professional</span>{" "}
-                or{" "}
-                <GradientTitle className="font-semibold tracking-wide bg-gradient-to-r from-blue-400 to-80% to-pink-500">
-                  Playful
-                </GradientTitle>
-              </span>
+              <ul>
+                <li>
+                  <span className="tracking-tight font-serif">Stylistic & Professional</span> or{" "}
+                  <GradientTitle className="font-semibold tracking-wide bg-gradient-to-r from-blue-400 to-80% to-pink-500">
+                    Playful
+                  </GradientTitle>
+                </li>
+              </ul>
             </li>
             <li className="mt-4">
               Easily use, adapt or build new features
               <ul>
-                <li className="text-4xl text-blue-900 italic">
-                  BTW: Did you notice this cool footer already? 👇
-                </li>
+                <li className="text-4xl text-blue-800">Did you already notice this cool footer? 👇</li>
               </ul>
             </li>
           </ul>
